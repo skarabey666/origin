9e8ac3f42bc989c6a5bb591a53177502c5603e13
Den <denis.jutov@gmail.com>
2018-04-12 23:39:40 +0800

    Ispravil oshibku v gitignore

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001523547092';
-$dateexpire = '001523550752';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1523547149;}';
+$datecreate = '001523547296';
+$dateexpire = '001523550956';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1523547353;}';
 return true;
 ?>
